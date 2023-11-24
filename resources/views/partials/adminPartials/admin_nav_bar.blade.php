33fca2ce4bac29d3d50867c0f96dc6607603ac49
Jagho <jaghobrel@gmail.com>
2023-11-25 00:35:16 +0100

    Integrating links in admin template

--- resources/views/partials/adminPartials/admin_nav_bar.blade.php
+++ resources/views/partials/adminPartials/admin_nav_bar.blade.php
@@ -1,3 +1,4 @@
+
 <div class="page-sidebar">
                 <!-- START X-NAVIGATION -->
                 <ul class="x-navigation">
@@ -25,7 +26,7 @@
                     </li>
                     <li class="xn-title">Navigation</li>
                     <li>
-                        <a href="#"><span class="fa fa-dashboard"></span> <span class="xn-text">Dashboards</span></a>
+                        <a href="home"><span class="fa fa-dashboard"></span> <span class="xn-text">Dashboard</span></a>
                     </li>                    
                     <li class="xn-openable">
                         <a href="#"><span class="fa fa-files-o"></span> <span class="xn-text">Pages</span></a>
@@ -33,6 +34,45 @@
                             <li><a href="adminResources/pages-edit-profile.html"><span class="fa fa-wrench"></span> Edit Profile</a></li>
                             <li><a href="adminResources/pages-profile.html"><span class="fa fa-user"></span> Profile</a></li>
                         </ul>
+                    </li>
+                    <li class="xn-openable">
+                        <a href="#"><span class="fa fa-home"></span> <span class="xn-text">Manage Home Page content</span></a>
+                        <ul>
+                            <li><a href="slides"><span class="fa fa-wrench"></span>Manage sliders</a></li>
+                            <li><a href="footer"><span class="fa fa-bookmark-o"></span>Manage footer</a></li>
+                        </ul>
+                    </li>
+                    <!--<li>
+                        <a href="updateHomePage"><span class="fa fa-home"></span> <span class="xn-text">Manage Home Page content</span></a>
+                    </li>-->
+                    <li class="xn-openable">
+                        <a href="#"><span class="fa fa-phone"></span> <span class="xn-text">Update contact information</span></a>
+                        <ul>
+                            <li><a href="updateContactDetails"><span class="fa fa-user"></span>Update Contact Details</a></li>
+                            <li><a href="regionallocations"><span class="fa fa-globe"></span>Update Regional Locations</a></li>
+                            <li><a href="faqs"><span class="fa fa-check-square"></span>Update FAQs</a></li>
+                        </ul>
+                    </li>
+                    <!--<li>
+                        <a href="updateContactUs"><span class="fa fa-phone"></span> <span class="xn-text">Update contact information</span></a>
+                    </li>-->
+                    <li>
+                        <a href="news"><span class="fa fa-info"></span> <span class="xn-text">Access News</span></a>
+                    </li>
+                    <li>
+                        <a href="updateExamination"><span class="glyphicon glyphicon-edit"></span> <span class="xn-text">Update Examination information</span></a>
+                    </li>
+                    <li>
+                        <a href="updateAboutTvet"><span class="fa fa-credit-card"></span> <span class="xn-text">Update About Tvet information</span></a>
+                    </li>
+                    <li>
+                        <a href="updateResources"><span class="glyphicon glyphicon-refresh"></span> <span class="xn-text">Update Resources</span></a>
+                    </li>
+                    <li>
+                        <a href="admintvetcollegetimes"><span class="glyphicon glyphicon-time"></span> <span class="xn-text">Manage TVET College Time</span></a>
+                    </li>
+                    <li>
+                        <a href="subscription"><span class="fa fa-users"></span> <span class="xn-text">View Subscriptions</span></a>
                     </li>
                 </ul>
                 <!-- END X-NAVIGATION -->
