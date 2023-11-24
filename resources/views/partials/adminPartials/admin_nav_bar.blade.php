47296e871e4625f1fa5478872c2f4f79594597bc
Jagho <jaghobrel@gmail.com>
2023-11-24 22:54:31 +0100

    Removing unused files

---
+++ resources/views/partials/adminPartials/admin_nav_bar.blade.php
@@ -0,0 +1,39 @@
+<div class="page-sidebar">
+                <!-- START X-NAVIGATION -->
+                <ul class="x-navigation">
+                    <li class="xn-logo">
+                        <a href="adminResources/index.html">ATLANT</a>
+                        <a href="#" class="x-navigation-control"></a>
+                    </li>
+                    <li class="xn-profile">
+                        <a href="#" class="profile-mini">
+                            <img src="adminResources/assets/images/users/avatar.jpg" alt="John Doe"/>
+                        </a>
+                        <div class="profile">
+                            <div class="profile-image">
+                                <img src="adminResources/assets/images/users/avatar.jpg" alt="John Doe"/>
+                            </div>
+                            <div class="profile-data">
+                                <div class="profile-data-name">John Doe</div>
+                                <div class="profile-data-title">Web Developer/Designer</div>
+                            </div>
+                            <div class="profile-controls">
+                                <a href="adminResources/pages-profile.html" class="profile-control-left"><span class="fa fa-info"></span></a>
+                                <a href="adminResources/pages-messages.html" class="profile-control-right"><span class="fa fa-envelope"></span></a>
+                            </div>
+                        </div>                                                                        
+                    </li>
+                    <li class="xn-title">Navigation</li>
+                    <li>
+                        <a href="#"><span class="fa fa-dashboard"></span> <span class="xn-text">Dashboards</span></a>
+                    </li>                    
+                    <li class="xn-openable">
+                        <a href="#"><span class="fa fa-files-o"></span> <span class="xn-text">Pages</span></a>
+                        <ul>
+                            <li><a href="adminResources/pages-edit-profile.html"><span class="fa fa-wrench"></span> Edit Profile</a></li>
+                            <li><a href="adminResources/pages-profile.html"><span class="fa fa-user"></span> Profile</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- END X-NAVIGATION -->
+            </div>
