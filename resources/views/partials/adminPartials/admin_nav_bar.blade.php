43682d65ba6439fcb01bbd08bc60ab42f02b2274
Jagho <jaghobrel@gmail.com>
2023-11-25 02:02:47 +0100

    Updates

--- resources/views/partials/adminPartials/admin_nav_bar.blade.php
+++ resources/views/partials/adminPartials/admin_nav_bar.blade.php
@@ -42,9 +42,6 @@
                             <li><a href="footer"><span class="fa fa-bookmark-o"></span>Manage footer</a></li>
                         </ul>
                     </li>
-                    <!--<li>
-                        <a href="updateHomePage"><span class="fa fa-home"></span> <span class="xn-text">Manage Home Page content</span></a>
-                    </li>-->
                     <li class="xn-openable">
                         <a href="#"><span class="fa fa-phone"></span> <span class="xn-text">Update contact information</span></a>
                         <ul>
@@ -53,20 +50,33 @@
                             <li><a href="faqs"><span class="fa fa-check-square"></span>Update FAQs</a></li>
                         </ul>
                     </li>
-                    <!--<li>
-                        <a href="updateContactUs"><span class="fa fa-phone"></span> <span class="xn-text">Update contact information</span></a>
-                    </li>-->
                     <li>
                         <a href="news"><span class="fa fa-info"></span> <span class="xn-text">Access News</span></a>
                     </li>
-                    <li>
+                    <li class="xn-openable">
                         <a href="updateExamination"><span class="glyphicon glyphicon-edit"></span> <span class="xn-text">Update Examination information</span></a>
+                        <ul>
+                            <li><a href="examinformationresources"><span class="fa fa-maxcdn"></span>Manage Examination Resources</a></li>
+                            <li><a href="examtrainingvideos"><span class="fa fa-video-camera"></span>Update Training Videos</a></li>
+                            <li><a href="icasstrainingvideos"><span class="fa fa-youtube-play"></span>Update ICASS Training Videos</a></li>
+                        </ul>
                     </li>
-                    <li>
+                    <li class="xn-openable">
                         <a href="updateAboutTvet"><span class="fa fa-credit-card"></span> <span class="xn-text">Update About Tvet information</span></a>
+                        <ul>
+                            <li><a href="adminAbouttvet"><span class="glyphicon glyphicon-plus"></span>Add about TVET</a></li>
+                            <li><a href="communitycolleges"><span class="glyphicon glyphicon-floppy-open"></span>Update Community Colleges</a></li>
+                            <li><a href="setas"><span class="glyphicon glyphicon-hand-right"></span>Update SETAs</a></li>
+                        </ul>
                     </li>
-                    <li>
+                    <li class="xn-openable">
                         <a href="updateResources"><span class="glyphicon glyphicon-refresh"></span> <span class="xn-text">Update Resources</span></a>
+                        <ul>
+                            <li><a href="collegecoursetype"><span class="glyphicon glyphicon-flash"></span>Manage College Course Type</a></li>
+                            <li><a href="collegecalendar"><span class="fa fa-calendar"></span>Manage Calendar</a></li>
+                            <li><a href="careerguidance"><span class="fa fa-caret-square-o-up"></span>Manage Carreer Guidance</a></li>
+                            <li><a href="successstories"><span class="glyphicon glyphicon-ok"></span>Manage Success Storiese</a></li>
+                        </ul>
                     </li>
                     <li>
                         <a href="admintvetcollegetimes"><span class="glyphicon glyphicon-time"></span> <span class="xn-text">Manage TVET College Time</span></a>
