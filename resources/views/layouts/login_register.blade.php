d165f09ecdd031bc07975be12cf9c0d91b1d7db3
Jagho <jaghobrel@gmail.com>
2023-11-26 10:58:43 +0100

    Redesigning login page

---
+++ resources/views/layouts/login_register.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" class="body-full-height">
+    <head>        
+        <!-- META SECTION -->
+        <title>{{ config('app.name') }}</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <!-- END META SECTION -->
+        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="adminResources/css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->                                     
+    </head>
+    <body>
+        
+        <div class="login-container login-v2">
+            
+            <div class="login-box animated fadeInDown">
+                <div class="login-body">
+                    <div class="login-title"><strong>Welcome</strong>, Please login.</div>
+                    @yield('content')
+                </div>
+                <div class="login-footer">
+                    <div class="pull-left">
+                        &copy; 2023 MICTSETA
+                    </div>
+                    <div class="pull-right">
+                        <a href="#">About</a> |
+                        <a href="#">Privacy</a> |
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
+    </body>
+</html>
+
+
+
+
+
+
