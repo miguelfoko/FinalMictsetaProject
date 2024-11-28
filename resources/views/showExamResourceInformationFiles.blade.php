ea502d79d01fc73dafc6f931e6452c7a5549f530
miguelfoko <miguelfoko@gmail.com>
2024-11-28 23:08:59 +0100

    Fixing display issue on Exam resource page: Ok

--- resources/views/showExamResourceInformationFiles.blade.php
+++ resources/views/showExamResourceInformationFiles.blade.php
@@ -10,53 +10,59 @@
             <div class="single-blog">
                 @switch($cat)
                         @case(1)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
-                                        <embed src="{{url('files/Exam_Entry_Form')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
+                                <h5><a href="{{url('files/Exam_Entry_Form')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                              <!--   <div class="theSiteFM">
+                                       <embed src="{{url('files/Exam_Entry_Form')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
+                                        
                                 </div>
-                                        <a href="{{url('files/Exam_Entry_Form')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                  -->      
                                 @break
                         @case(2)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
-                                        <embed src="{{url('files/Examination_Re_Marking')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
+                                <h5><a href="{{url('files/Examination_Re_Marking')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                                <!--<div class="theSiteFM">
+                                        <embed src="{{url('files/Examination_Re_Marking')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">-->
                                 </div>
-                                <a href="{{url('files/Examination_Re_Marking')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                -->
                                 @break          
                         @case(3)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
+                                <h5><a href="{{url('files/Examination_Time_table')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                                <!--<div class="theSiteFM">
                                         <embed src="{{url('files/Examination_Time_table')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
                                 </div>
-                                <a href="{{url('files/Examination_Time_table')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                -->
+                                
                                 @break
                         @case(4)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
+                                <h5><a href="{{url('files/General_Items')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                                <!--<div class="theSiteFM">
                                         <embed src="{{url('files/General_Items')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
                                 </div>
-                                <a href="{{url('files/General_Items')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                -->
+                                
                                 @break
                         @case(5)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
+                                <h5><a href="{{url('files/Icass_Support_Documents')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                               <!--<div class="theSiteFM">
                                         <embed src="{{url('files/Icass_Support_Documents')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
                                 </div>
-                                <a href="{{url('files/Icass_Support_Documents')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                -->
+                                
                                 @break
                         @case(6)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
+                                <h5><a href="{{url('files/Management_Action_Planning')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                                <!--<div class="theSiteFM">
                                         <embed src="{{url('files/Management_Action_Planning')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
                                 </div>
-                                <a href="{{url('files/Management_Action_Planning')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                -->
+                                
                                 @break
                         @case(7)
-                                <h5>{{$examresource->name}} </h5>
-                                <div class="theSiteFM">
+                                <h5><a href="{{url('files/Validation_Programmes')}}/{{$examresource->file}}" target="_blank" rel="noopener">{{$examresource->name}}</a> </h5>
+                                <!--<div class="theSiteFM">
                                         <embed src="{{url('files/Validation_Programmes')}}/{{$examresource->file}}" type="application/pdf" height="250" width="200">
                                 </div>
-                                <a href="{{url('files/Validation_Programmes')}}/{{$examresource->file}}" target="_blank" rel="noopener">Download</a>
+                                -->
+                                
                                 @break
                 @endswitch
           </div>
